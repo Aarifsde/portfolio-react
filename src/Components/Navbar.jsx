76316125abb485dce15530b3ae79a207c4053b8f
Aarif <aarifsde@gmail.com>
2023-09-29 09:33:28 +0530

    profile link update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
             <ul>
                 <li className='w-[180px] h-[60px] flex justify-between items-center ml-[-120px] hover:ml-[-10px] duration-300 bg-blue-600'>
-                    <a href='https://www.linkedin.com/in/aarif-a-sde' target='_blank' rel="noreferrer" className='flex justify-between items-center w-full text-gray-300'>
+                    <a href='www.linkedin.com/in/aarif-webdev' target='_blank' rel="noreferrer" className='flex justify-between items-center w-full text-gray-300'>
                         Linkedin <FaLinkedin size={30} />
                     </a>
                 </li>
